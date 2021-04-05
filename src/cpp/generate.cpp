b873a4575887ccde2bdc449a3d8f688f77fdf880
Nicolas Barrios <nbarrios1337@gmail.com>
2021-04-05 16:29:35 -0400

    Reference grid iteration

--- src/cpp/generate.cpp
+++ src/cpp/generate.cpp
@@ -1,17 +1,22 @@
 #include <bitset>
 #include <iostream>
 #include <random>
+#include <cstdio>
 
 #define GRID_DIM 1024
 
 // ORNL Code uses a 1D grid
-int dim = 32;
+int x_dim = 4;
+int y_dim = x_dim * sizeof(int);
 
 int *generate() {
-    int *grid = (int *)calloc((dim + 2) * (dim + 2), sizeof(int));
-    for (int i = 1; i <= dim; i++) {
-        for (int j = 1; j <= dim; j++) {
-            grid[i * (dim + 2) + j] = rand() % 2;
+    int *grid = (int *)calloc((y_dim + 2) * (x_dim + 2), sizeof(int));
+    //int *grid = new int[(y_dim + 2) * (x_dim + 2)];
+    printf("%p\n", grid);
+    //  i \in [1, dim+1)
+    for (int i = 1; i <= y_dim; i++) {
+        for (int j = 1; j <= x_dim; j++) {
+            grid[i * (x_dim + 2) + j] = 1;
         }
     }
     return grid;
@@ -19,13 +24,14 @@ int *generate() {
 
 int main() {
     int *grid = generate();
-    for (int i = 1; i <= dim; i++) {
-        for (int j = 1; j <= dim; j++) {
-            std::cout << grid[i * (dim + 2) + j];
+    for (int i = 1; i <= y_dim; i++) {
+        for (int j = 1; j <= x_dim; j++) {
+            std::cout << grid[i * (x_dim + 2) + j];
         }
         std::cout << std::endl;
     }
 
     free(grid);
+    //delete [] (grid);
     return 0;
 }
